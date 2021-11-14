00e2162447257826ab4f60d06a660c30aeba6a34
zarkisd <75956855+zarkisd@users.noreply.github.com>
2021-11-14 20:37:19 +0900

    Initial commit

---
+++ app/src/main/java/com/example/basicsyntax/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.basicsyntax
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        var name: String
+        name = "test"
+        Log.d("BasicSyntax", "name is $name")
+    }
+}
